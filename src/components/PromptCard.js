96f19f1d60aa262543c36a02af2127fa3dd9f636
Bard of Code <harnettmusic@gmail.com>
2024-01-13 15:11:55 -0330

    websocket setup 1.0

--- src/components/PromptCard.js
+++ src/components/PromptCard.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
 import axios from "axios";
+import useWebSocket from "../util/UseWebSocket";
 
 const PromptCard = ({ performanceCode }) => {
   const [prompt, setPrompt] = useState(null);
@@ -10,21 +11,38 @@ const PromptCard = ({ performanceCode }) => {
   const [error, setError] = useState(null);
   const [logPrompt, setLogPrompt] = useState(null);
 
-  const fetchPrompt = async () => {
-    try {
-      const response = await axios.get(`${process.env.REACT_APP_PROMPT_API}`);
-      setPrompt(response.data);
-      const startTime = new Date().toISOString();
-      setStartTime(startTime);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setError(error);
+  const ws = useWebSocket({ setPrompt });
+
+  const sendMessage = (message) => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(message);
     }
   };
 
-  useEffect(() => {
-    fetchPrompt();
-  }, []);
+  const onMessageReceived = (event) => {
+    try {
+      const receivedPrompt = JSON.parse(event.data);
+      setPrompt(receivedPrompt);
+      setStartTime(new Date().toIseString());
+    } catch (e) {
+      setError(e);
+    }
+  };
+
+  // const fetchPrompt = async () => {
+  //   try {
+  //     const response = await axios.get(`${process.env.REACT_APP_PROMPT_API}`);
+  //     setPrompt(response.data);
+  //     const startTime = new Date().toISOString();
+  //     setStartTime(startTime);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //     setError(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchPrompt();
+  // }, []);
 
   const handleIgnorePrompt = () => {
     setIgnore(true);
@@ -40,18 +58,20 @@ const PromptCard = ({ performanceCode }) => {
       endTime: newEndTime,
       ignore: ignore,
     });
-    fetchPrompt();
+    sendMessage(JSON.stringify({ action: "sendPrompt" }));
     setIgnore(false);
   };
 
   if (error) return <div>Error loading data.</div>;
-  if (!prompt) return <div>Loading...</div>;
 
   return (
     <>
       <Card style={{ width: "50rem" }} className="m-5 text-center">
         <Card.Body className="align-items-center">
-          <Card.Title className="fs-1">{prompt.body.Prompt}</Card.Title>
+          {prompt && (
+            <Card.Title className="fs-1">{prompt.body.Prompt}</Card.Title>
+          )}
+          {!prompt && <Card.Title className="fs-1">Loading...</Card.Title>}
           <Card.Text className=""></Card.Text>
           <Button type="submit" className="mx-2" onClick={handleNextPrompt}>
             Next Prompt

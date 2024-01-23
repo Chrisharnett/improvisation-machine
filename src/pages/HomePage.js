785ec34f45fce7632fae9d28877df7a48cc110fb
Bard of Code <harnettmusic@gmail.com>
2024-01-23 11:25:20 -0330

    piped

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <h1> This is the improvisation prompting machine </h1>
+      <h1> The A5tral8og improvisation prompting machine </h1>
       <br></br>
       <h2> Join a performance to participate </h2>
       <br></br>

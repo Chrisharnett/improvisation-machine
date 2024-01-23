d3ea3d0ffe44667fae9f52f0473ff4fca5318a6b
Bard of Code <harnettmusic@gmail.com>
2024-01-23 10:37:09 -0330

    strange

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <h1> This is an improvisation prompting machine </h1>
+      <h1> This is the improvisation prompting machine </h1>
       <br></br>
       <h2> Join a performance to participate </h2>
       <br></br>

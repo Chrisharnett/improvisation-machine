b438bd4d6d626a86c66561c6d22614f2127ca6c4
Bard of Code <harnettmusic@gmail.com>
2024-01-23 14:20:08 -0330

    pipin tests

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,17 @@ const HomePage = () => {
   return (
     <>
       <h1> Welcome to the A5tral8og improvisation prompting machine </h1>
-      <br></br>
-      <h2> Join a performance and start playing. </h2>
+      <hr></hr>
+      This app gives customized prompts to improvisers to help direct the
+      creation of spontaneous music. This is the first prototype is designed to
+      deliver randomized prompts from a bank of available prompts. Once this
+      proof of concept is refined, performers will be given a choice to prompts
+      to choose from to create a database of "preferred" prompts. That dataset
+      will ideally inform a machine learning model to automate the delivery of
+      prompts to users. As that machine learning model grows, the app will
+      collect audience feedback during performance to further refine the model
+      choices.
+      <h2> Join a performance to start playing. </h2>
       <br></br>
       <h2> Login to launch a performance or design prompts. </h2>
       <br></br>
